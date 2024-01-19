9524f16d3768bb4b92e71dbcb6e4fb5ae77872a7
Teemu Höytö <teemu@redandblue.fi>
2024-01-19 21:49:29 +0200

    testing fetch api

---
+++ fetch_template_part.js
@@ -0,0 +1,12 @@
+// fetchTemplatePart.js
+
+function fetchTemplatePart(templateName) {
+  fetch(`vue_apps/${templateName}.html`)
+      .then(response => response.text())
+      .then(html => {
+          // Insert the JavaScript content into the div that contains the script
+          const scriptContainer = document.currentScript.parentElement;
+          scriptContainer.innerHTML = html;
+      })
+      .catch(error => console.error(`Error fetching HTML for ${templateName}:`, error));
+}
