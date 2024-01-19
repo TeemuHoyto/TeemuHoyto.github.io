dcdc13c7884b7856e26e02e26f197adb2b97e903
Teemu Höytö <teemu@redandblue.fi>
2024-01-19 21:53:03 +0200

    tweaks to js and HTML

--- fetch_template_part.js
+++ fetch_template_part.js
@@ -5,7 +5,7 @@ function fetchTemplatePart(templateName) {
       .then(response => response.text())
       .then(html => {
           // Insert the JavaScript content into the div that contains the script
-          const scriptContainer = document.currentScript.parentElement;
+          const scriptContainer = document.getElementById(templateName);
           scriptContainer.innerHTML = html;
       })
       .catch(error => console.error(`Error fetching HTML for ${templateName}:`, error));
